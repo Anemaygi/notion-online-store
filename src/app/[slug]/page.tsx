a589ed021e5a42e3b83be90e0e91ff83cf118159
Anemaygi <gianemayumisan@gmail.com>
2025-01-22 16:54:51 -0300

    fix: build

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -3,17 +3,20 @@ import { Item } from "../../../@types/schema";
 import { ItemPage } from "./ItemPage";
 
 interface ItemCardProps {
-    item: Item; 
-  }
+  item: Item;
+}
 
-export default async function Page({ params, }: { params: { slug: string } }) {
-  
-  const { slug } = params
+interface PageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default async function Page({ params }: PageProps) {
+  const { slug } = params;
   const notionService = new NotionService();
-  const itemPage = await notionService.getSingleItem(slug)
-  const item = itemPage.item; 
+  const itemPage = await notionService.getSingleItem(slug);
+  const item = itemPage.item;
 
-  return (
-    <ItemPage item={item} />
-  )
+  return <ItemPage item={item} />;
 }
