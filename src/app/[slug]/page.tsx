24935edc02996d2606282faa6c07eaf0d339448e
Anemaygi <gianemayumisan@gmail.com>
2025-01-22 16:56:06 -0300

    fix: build

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { GetStaticProps } from 'next';
 import NotionService from "../../../services/notion-service";
 import { Item } from "../../../@types/schema";
 import { ItemPage } from "./ItemPage";
@@ -7,16 +8,21 @@ interface ItemCardProps {
 }
 
 interface PageProps {
-  params: {
-    slug: string;
-  };
+  item: Item;
 }
 
-export default async function Page({ params }: PageProps) {
-  const { slug } = params;
+export const getStaticProps: GetStaticProps<PageProps, { slug: string }> = async ({ params }) => {
   const notionService = new NotionService();
-  const itemPage = await notionService.getSingleItem(slug);
+  const itemPage = await notionService.getSingleItem(params?.slug ?? '');
   const item = itemPage.item;
 
+  return {
+    props: {
+      item,
+    },
+  };
+};
+
+export default function Page({ item }: PageProps) {
   return <ItemPage item={item} />;
 }
